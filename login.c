45cff16ff6e36680c8dd29a3a77cf1abafd08f53
JimmyArgote <31542706+JimmyArgote@users.noreply.github.com>
2018-11-20 17:55:10 -0200

    update new-pim-3(19-11-18)
    update new-pim-3(19-11-18)

--- login.c
+++ login.c
@@ -1,30 +1,49 @@
-
+#include <stdio.h>
+#include <time.h>
 /* lg = login, ps = password */
-/* função de atenticação*/
-int logar(char lg[30], char ps[30])
-{
-    int auth;
-    if( (lg && ps) != NULL )
-    {
-        if(strcmp(lg, ps) == 0)
-        {
-            auth = TRUE;
-        }
-    }
+/* funÃ§Ã£o de atenticaÃ§Ã£o*/
 
-    return auth;
-}
+//#define CLOCKS_PER_SEC 10
 
-exit()
-{
-    return;
-}
 
 int timeout ( int seconds )
 {
     clock_t endwait;
     endwait = clock () + seconds * CLOCKS_PER_SEC ;
-    while (clock() < endwait) {}
+    while (clock() < endwait) {
+        //remaining(seconds);
+    }
 
-    return  1;
+    return 1;
 }
+
+
+remaining(int seconds )
+{
+    int tempo = 0, hora = 0, minuto = 0, segundo = seconds;
+
+   while(tempo < 10)
+   {
+       printf("CRONOMEOTRP:\n\n");
+       printf("%d:%d:%d\n\n", hora, minuto, segundo);
+       //sleep(1000);
+       system("clear");
+       segundo++;
+
+       if(segundo == 60)
+       {
+           segundo = 0;
+           minuto++;
+       }
+       if(minuto == 60)
+       {
+           minuto = 0;
+           hora++;
+       }
+       if(hora == 24)
+       {
+            hora = 0;
+       }
+   }
+}
+
